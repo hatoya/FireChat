f63c48b3868983689a0b4ee99cd010e25e071788
WadaHayato <hatoya.web.design@gmail.com>
2014-11-24 14:15:59 +0900

    connect firebase

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,29 @@
-var firebaseURL = "https://fiery-torch-2959.firebaseio.com/";
+$(function(){
+    var firebaseURL = new Firebase("https://fiery-torch-2959.firebaseio.com/");
+
+    var nameField = $('#userName');
+    var inputField = $('#inputContent');
+    var contentField = $('.content ul');
+
+    inputField.keypress(function(e){
+        if(e.keyCode == 13){
+            var userName = nameField.val();
+            var content = inputField.val();
+
+            firebaseURL.push({name: userName, message: content});
+            inputField.val('');
+        }
+    });
+
+    firebaseURL.limit(10).on('child_added', function(content){
+        var userName = content.val().name;
+        var message = content.val().message;
+
+        var nameElement = $('<span>').text(userName);
+        var messageElement = $('<li>').text(message).prepend(nameElement);
+
+        contentField.append(messageElement);
+
+        contentField[0].scrollTop = contentField[0].scrollHeight;
+    });
+});
